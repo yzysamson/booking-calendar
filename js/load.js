5e8220576bb27d267c2c137ebf3ea5d3ff815ee3
yzysamson <yzy.samson@gmail.com>
2025-12-25 01:19:23 +0800

    .

--- js/load.js
+++ js/load.js
@@ -1,12 +1,1 @@
-async function loadAll(){
-  ROOMS=(await sb.from('rooms').select('*').order('id')).data||[];
-  BOOKINGS=(await sb.from('booking_view').select('*')).data||[];
-  BOOKINGS.forEach(b=>delete b.__hidden);
-
-  buildLegend();
-  buildDays();
-  buildSelects();
-  render();
-  renderYAxis();
-  renderSummary();
-}
+// load.js disabled – logic moved to calendar.js
